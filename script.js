8319bf14d155ee9a057a7efb8f698c4317f1ecd0
Tienhcmus <60434436+Tienhcmus@users.noreply.github.com>
2020-03-28 22:04:48 +0700

    Upload html, css and js

---
+++ script.js
@@ -0,0 +1,9 @@
+let header = document.querySelector('.menu');
+let origOffsetY = header.offsetTop;
+
+function onScroll(e) {
+    window.scrollY >= origOffsetY ? header.classList.add('sticky') :
+        header.classList.remove('sticky');
+}
+
+document.addEventListener('scroll', onScroll);
