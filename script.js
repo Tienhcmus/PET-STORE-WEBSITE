2b5b470e00c723eec5931e310bded3cb9d634e71
tienminh13 <pnmtien0301@outlook.com>
2020-04-18 18:50:29 +0700

    Update Validation Signup form

--- script.js
+++ script.js
@@ -4,6 +4,4 @@ let origOffsetY = header.offsetTop;
 function onScroll(e) {
     window.scrollY >= origOffsetY ? header.classList.add('sticky') :
         header.classList.remove('sticky');
-}
-
-document.addEventListener('scroll', onScroll);
+};
