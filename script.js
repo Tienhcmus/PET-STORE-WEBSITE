e95c3cad0e26a79317b07b11bedd6047e30f4b07
tienminh13 <pnmtien0301@outlook.com>
2020-05-09 19:03:38 +0700

    update cart detail

--- script.js
+++ script.js
@@ -6,3 +6,61 @@ function onScroll(e) {
         header.classList.remove('sticky');
 };
 document.addEventListener('scroll', onScroll);
+
+const $tableID = $('#table');
+$tableID.on('click', '.table-remove', function () {
+
+    $(this).parents('tr').detach();
+});
+
+sum();
+
+$('.plus').click(function (e) {
+    e.preventDefault();
+    let sp = parseFloat($(this).prev('span').text());
+    $(this).prev('span').text(sp + 1);
+    sumproduct++;
+    $('#sumproduct').html(sumproduct);
+    sum();
+});
+
+$('.minus').click(function (e) {
+    e.preventDefault();
+    let sp = parseFloat($(this).next('span').text());
+    $(this).next('span').text(sp - 1);
+    if (!isNaN(sp) && sp > 0) {
+        $(this).next('span').text(sp - 1);
+        sumproduct--;
+        $('#sumproduct').html(sumproduct);
+        sum();
+    } else {
+        $(this).next('span').text(0);
+    }
+});
+
+$("[id*=remove]").each(function () {
+    $(this).click(function () {
+        $(this).parent().parent().remove();
+        sum();
+    })
+})
+
+function sum() {
+    let cash = 0;
+    let sumproduct = 0;
+    $('tr').each(function () {
+        let price = $(this).find('#cellprice').text();
+        let amount = $(this).find('#cellamount').text();
+        $(this).find('#cellsum').html(price * amount);
+    });
+    $("[id*=cellsum]").each(function () {
+        cash += parseFloat($(this).text());
+    })
+    $('#sum').html(cash + ' VND');
+    
+    $("[id*=cellamount]").each(function () {
+        sumproduct += parseFloat($(this).text());
+    })
+    $('#sumproduct').html(sumproduct);
+}
+
